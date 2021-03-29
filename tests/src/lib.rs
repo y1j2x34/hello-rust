34589859503ddc4f6b06ee94c6d34d3c02b35a14
y1j2x34 <y1j2x34@qq.com>
2021-03-30 01:00:16 +0800

    tests

---
+++ tests/src/lib.rs
@@ -0,0 +1,62 @@
+
+pub fn greeting(name: &str) -> String {
+    format!("Hello {}", name)
+}
+
+pub struct Guess {
+    value: i32,
+}
+
+impl Guess {
+    pub fn new(value: i32) -> Guess {
+        if value < 1 || value > 100 {
+            panic!("Guess value must be between 1 amd 100, got {}.", value);
+        }
+        Guess {
+            value
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+    #[test]
+    fn it_works_r() -> Result<(), String> {
+        if 2 + 2 == 4 {
+            Ok(())
+        } else {
+            Err(String::from("two plus two does not equal to four"))
+        }
+    }
+    #[test]
+    fn greeting_contains_name() {
+        let result = greeting("Carol");
+        assert!(
+            result.contains("Carol"),
+            "Greeting did not contain name, value was: {}", result
+        )
+    }
+    #[test]
+    fn greeting_contains_name2() {
+        let result = greeting("Carol");
+        assert!(
+            result.contains("Carol0"),
+            "Greeting did not contain name, value was: {}", result
+        )
+    }
+    #[test]
+    #[should_panic(expected = "Guess value must be between 1 amd 100")] // panic 子串
+    fn greater_than_100() {
+        Guess::new(200);
+    }
+    #[test]
+    fn another() {
+        panic!("Make this test fail")
+    }
+}
