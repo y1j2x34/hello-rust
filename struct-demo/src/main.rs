601ec24312e218499553fba3e7bdbf8e68a2cf99
y1j2x34 <y1j2x34@qq.com>
2022-04-03 20:05:06 +0800

    structs

--- struct-demo/src/main.rs
+++ struct-demo/src/main.rs
@@ -6,6 +6,13 @@ struct User {
 
 //  语法糖
 impl User {
+    fn default_user() -> Self {
+        Self {
+            username: String::from("default user"),
+            age: 1,
+            email: String::from("default email")
+        }
+    }
     fn show_user(self: &User) {
         println!("name: {}, age: {}, email: {}", self.username, self.age, self.email);
     }
@@ -23,4 +30,17 @@ fn main() {
     };
     show_user(&someone);
     someone.show_user();
+
+    User::default_user().show_user();
+
+
+    let A(a, b) = A(1,2);
+    add(a, b);
+}
+
+
+struct A(i32,i32);
+
+fn add(a: i32, b: i32) -> i32 {
+    a + b
 }
